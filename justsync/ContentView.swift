d5b3548f7c3f35e447cadaad46bbcf1e6ab0f72f
gaoxingliang@outlook.com <gaoxingliang@outlook.com>
2026-06-15 08:57:59 +0800

    Initial Commit

---
+++ justsync/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  justsync
+//
+//  Created by edward on 2026/6/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ justsync/justsyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  justsyncApp.swift
+//  justsync
+//
+//  Created by edward on 2026/6/15.
+//
+
+import SwiftUI
+
+@main
+struct justsyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
